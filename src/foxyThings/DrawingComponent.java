2d4189cc3854c87d2355c56d8b1c8a74adcb5e29
Yonatan Nitzan <Yognatan33@gmail.com>
2017-01-17 20:11:18 +0200

    Polished lines showing correctly

--- src/foxyThings/DrawingComponent.java
+++ src/foxyThings/DrawingComponent.java
@@ -4,114 +4,66 @@ import java.awt.Color;
 import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.Image;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
 import javax.swing.JComponent;
 import javax.swing.Timer;
 
 import java.awt.geom.Ellipse2D;
-import java.util.LinkedList;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
 
 public class DrawingComponent extends JComponent implements ActionListener{
 	private static final long serialVersionUID = 1L;
-
-	LinkedList<String> line1 = new LinkedList<String>();
-	LinkedList<String> line2 = new LinkedList<String>();
 	
-	LinkedList<Color> line1C = new LinkedList<Color>();
-	LinkedList<Color> line2C = new LinkedList<Color>();
-	
+	Graphics2D g2;
 	int s = 0;
+	String line1I, line2I;
+	Color line1CI, line2CI;
+	Timer tm = new Timer(3000, this);
+	
+	BufferedImage sun;
 	
 	public void paintComponent(Graphics g)
 	{
-		line1.add("Sophia,");
-		line2.add("my dearest Fox.");
-		line1C.add(new Color(255, 0, 0));
-		line2C.add(new Color(255, 0, 0));
-		
-		line1.add("You are my sunshine,");
-		line2.add("the one to lighten up my day.");
-		line1C.add(new Color(255, 215, 0));
-		line2C.add(new Color(255, 215, 0));
-		
-		line1.add("The fuel of my happiness,");
-		line2.add("because all i need to be happy is you.");
-		line1C.add(new Color(255, 0, 0));
-		line2C.add(new Color(255, 0, 0));
-		
-		line1.add("You're mine,");
-		line2.add("forever and always.");
-		line1C.add(new Color(255, 0, 0));
-		line2C.add(new Color(255, 0, 0));
-		
-		line1.add("I'm yours,");
-		line2.add("forever and always.");
-		line1C.add(new Color(255, 0, 0));
-		line2C.add(new Color(255, 0, 0));
-		
-		line1.add("Two hearts beating as one,");
-		line2.add("striving to be closer.");
-		line1C.add(new Color(255, 0, 0));
-		line2C.add(new Color(255, 0, 0));
-		
-		line1.add("Two hearts connected by love,");
-		line2.add("that connection only growing with time...");
-		line1C.add(new Color(255, 0, 0));
-		line2C.add(new Color(255, 0, 0));
-		
-		line1.add("Every week,");
-		line2.add("Every day,");
-		line1C.add(new Color(255, 0, 0));
-		line2C.add(new Color(255, 0, 0));
-		
-		line1.add("Every minute,");
-		line2.add("Every Second,");
-		line1C.add(new Color(255, 0, 0));
-		line2C.add(new Color(255, 0, 0));
-		
-		line1.add("My love for you only grows stronger,");
-		line2.add("our connection grows tighter.");
-		line1C.add(new Color(255, 0, 0));
-		line2C.add(new Color(255, 0, 0));
-		
-		line1.add("Every moment in my life i want to spend with you,");
-		line2.add("My love.");
-		line1C.add(new Color(255, 0, 0));
-		line2C.add(new Color(255, 0, 0));
-		
-		line1.add("So will you be mine?");
-		line2.add("Officially?");
-		line1C.add(new Color(255, 0, 0));
-		line2C.add(new Color(255, 0, 0));
-		
-		line1.add("John, your dork,");
-		line2.add("signing out.");
-		line1C.add(new Color(255, 0, 0));
-		line2C.add(new Color(255, 0, 0));
-
-		
-		Graphics2D g2 = (Graphics2D) g;
+		g2 = (Graphics2D) g;
 		g2.setFont(new Font("serif", Font.BOLD, 20));
 		
 		SlideBg(g2);
 		
-		g2.setColor(line1C.get(s));
-		g2.drawString(line1.get(s), 300, 300);
+		try {
+			sun = ImageIO.read(getClass().getResourceAsStream("/SunDrawing.png"));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 		
-		g2.setColor(line2C.get(s));
-		g2.drawString(line2.get(s), 300, 350);
+		if(Main.iterL1.hasNext() && Main.iterL2.hasNext() && Main.iterL1C.hasNext() && Main.iterL2C.hasNext())
+		{
+			line1I = Main.iterL1.next();
+			line1CI = Main.iterL1C.next();
+			line2I = Main.iterL2.next();
+			line2CI = Main.iterL2C.next();
+		}
+		else
+			tm.stop();
 		
-		Timer tm = new Timer(5000, this);
+		g2.setColor(line1CI);
+		g2.drawString(line1I, 200, 300);
+		
+		g2.setColor(line2CI);
+		g2.drawString(line2I, 200, 350);
 		
 		tm.start();
 	}
 
 	@Override
 	public void actionPerformed(ActionEvent arg0) {
-		repaint();
 		s++;
+		repaint();
 	}
 	
 	private void SlideBg(Graphics2D g2)
@@ -128,14 +80,7 @@ public class DrawingComponent extends JComponent implements ActionListener{
 			g2.drawLine(75, 75, 100, 200);
 			g2.drawLine(75, 75, 200, 200);
 		default:
-			g2.setColor(new Color(255, 205, 0));
-			
-			Ellipse2D.Double el11 = new Ellipse2D.Double(0, 0, 150, 150);
-			g2.fill(el11);
-			
-			g2.drawLine(75, 75, 150, 200);
-			g2.drawLine(75, 75, 100, 200);
-			g2.drawLine(75, 75, 200, 200);
+			g2.drawImage(sun, 0, 0, null);
 		}
 	}
 	
--- src/foxyThings/Main.java
+++ src/foxyThings/Main.java
@@ -1,12 +1,99 @@
 package foxyThings;
 
+import java.awt.Color;
+import java.util.LinkedList;
+import java.util.ListIterator;
+
 import javax.swing.JFrame;
 
 public class Main {
-	public static void main(String[] args){
+	static LinkedList<String> line1 = new LinkedList<String>();
+	static LinkedList<String> line2 = new LinkedList<String>();
+	
+	static ListIterator<String> iterL1;
+	static ListIterator<String> iterL2;
+	
+	static LinkedList<Color> line1C = new LinkedList<Color>();
+	static LinkedList<Color> line2C = new LinkedList<Color>();
+	
+	static ListIterator<Color> iterL1C;
+	static ListIterator<Color> iterL2C;
+	
+	public static void main(String[] args){	
+		line1.add("Sophia,");
+		line2.add("my dearest Fox.");
+		line1C.add(new Color(255, 0, 0));
+		line2C.add(new Color(255, 0, 0));
+		
+		line1.add("You are my sunshine,");
+		line2.add("the one to lighten up my day.");
+		line1C.add(new Color(255, 215, 0));
+		line2C.add(new Color(255, 215, 0));
+		
+		line1.add("The fuel of my happiness,");
+		line2.add("because all i need to be happy is you.");
+		line1C.add(new Color(0, 255, 0));
+		line2C.add(new Color(0, 255, 0));
+		
+		line1.add("You're mine,");
+		line2.add("forever and always.");
+		line1C.add(new Color(0, 0, 255));
+		line2C.add(new Color(0, 0, 255));
+		
+		line1.add("I'm yours,");
+		line2.add("forever and always.");
+		line1C.add(new Color(255, 0, 0));
+		line2C.add(new Color(255, 0, 0));
+		
+		line1.add("Two hearts beating as one,");
+		line2.add("striving to be closer.");
+		line1C.add(new Color(0, 255, 0));
+		line2C.add(new Color(0, 255, 0));
+		
+		line1.add("Two hearts connected by love,");
+		line2.add("that connection only growing with time...");
+		line1C.add(new Color(0, 0, 255));
+		line2C.add(new Color(0, 0, 255));
+		
+		line1.add("Every week,");
+		line2.add("Every day,");
+		line1C.add(new Color(255, 0, 0));
+		line2C.add(new Color(255, 0, 0));
+		
+		line1.add("Every minute,");
+		line2.add("Every Second,");
+		line1C.add(new Color(0, 255, 0));
+		line2C.add(new Color(0, 255, 0));
+		
+		line1.add("My love for you only grows stronger,");
+		line2.add("our connection grows tighter.");
+		line1C.add(new Color(0, 0, 255));
+		line2C.add(new Color(0, 0, 255));
+		
+		line1.add("Every moment in my life i want to spend with you,");
+		line2.add("My love.");
+		line1C.add(new Color(255, 0, 0));
+		line2C.add(new Color(255, 0, 0));
+		
+		line1.add("So will you be mine?");
+		line2.add("Officially?");
+		line1C.add(new Color(0, 0, 0));
+		line2C.add(new Color(0, 0, 0));
+		
+		line1.add("John, your dork,");
+		line2.add("signing out.");
+		line1C.add(new Color(10, 150, 103));
+		line2C.add(new Color(25, 200, 102));
+		
+		iterL1 = line1.listIterator();
+		iterL2 = line2.listIterator();
+		
+		iterL1C = line1C.listIterator();
+		iterL2C = line2C.listIterator();
+		
 		JFrame window = new JFrame();
 		window.setSize(640, 480);
-		window.setTitle("Bla Bla");
+		window.setTitle("Lovy dovy dub dub!");
 		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		window.setVisible(true);
 		
